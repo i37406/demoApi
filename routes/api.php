53638e61f500016187cfc412339842235591fbc0
i37406 <waqas.ciitwah@gmail.com>
2021-07-05 22:05:59 +0500

    Create Model,Factories,Seeder,Migrations & Controllers. Also Create Resource Routes for Api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Routes for demoApi
+Route::apiresource('/products', ProductController::class);
+Route::prefix('products')->group(function () {
+    Route::apiresource('/{product}/reviews', ReviewController::class);
 });
