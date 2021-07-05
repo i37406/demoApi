17d3b9cf5918cc6ec7dc598b6b85da84eaf6a536
i37406 <waqas.ciitwah@gmail.com>
2021-07-05 22:29:41 +0500

    Create factory for Model and seed it

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -21,8 +21,13 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        //github faker library
         return [
-            //
+            'name' => $this->faker->name(),
+            'detail' => $this->faker->paragraph,
+            'price' => $this->faker->numberBetween(100,1000),
+            'stock' => $this->faker->randomDigit,
+            'discount' => $this->faker->numberBetween(2,30)
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //Seeds for our demoApi models
+        \App\Models\Product::factory(50)->create();
+        \App\Models\Review::factory(300)->create();
     }
 }
